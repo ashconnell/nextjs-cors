6b9cc41157bf2ecf4f0d9b5de4fcf937e434ebe9
Ash <hello@ashconnell.com>
2023-02-27 11:42:51 +1100

    add cors endpoint

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const cors = Cors();
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(cors);
+  res.status(200).json({ name: "John Doe" });
 }
